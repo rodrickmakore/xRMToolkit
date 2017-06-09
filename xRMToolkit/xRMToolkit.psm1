8750ebd39f5e8ea86ab16dfc877cebf311d7660d
Rodrick Makore <rodrick@live.co.za>
2017-06-09 03:29:19 +0200

    modified module

--- xRMToolkit/xRMToolkit.psm1
+++ xRMToolkit/xRMToolkit.psm1
@@ -4,3 +4,5 @@
 function Get-Function {
 	(Get-WmiObject Win32_OperatingSystem).__Class
 }
+
+Export-ModuleMember -Function Get-Function
--- xRMToolkit/xRMToolkit.tests.ps1
+++ xRMToolkit/xRMToolkit.tests.ps1
@@ -3,6 +3,7 @@
 # You need a unit test framework such as Pester to run PowerShell Unit tests. 
 # You can download Pester from http://go.microsoft.com/fwlink/?LinkID=534084
 #
+Import-Module .\xRMToolkit.psm1
 
 Describe "Get-Function" {
 	Context "Function Exists" {
